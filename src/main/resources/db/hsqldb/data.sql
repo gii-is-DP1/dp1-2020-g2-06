3c980701ac094d67e9b3b90bafa588c9bde65abc
Juanra <juanramonor@hotmail.com>
2020-10-20 00:20:20 +0200

    Correcion de owner Juanra

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -12,7 +12,7 @@ INSERT INTO users(username,password,enabled) VALUES ('jesapaort','passjesus',TRU
 INSERT INTO authorities(id,username,authority) VALUES (4,'jesapaort','owner');
 
 INSERT INTO users(username,password,enabled)VALUES('juanrauvu', 'passjuanra', TRUE)
-INSERT INTO authorities(id,username,authority) VALUES (5,'juanracat','owner');
+INSERT INTO authorities(id,username,authority) VALUES (5,'juanrauvu','owner');
 
 INSERT INTO users(username,password,enabled)VALUES('alebarled', 'passalebarled', TRUE)
 INSERT INTO authorities(id,username,authority) VALUES (6,'alebarled','owner');
@@ -55,6 +55,7 @@ INSERT INTO owners VALUES (10, 'Carlos', 'Estaban', '2335 Independence La.', 'Wa
 
 INSERT INTO owners VALUES (11, 'Jesus', 'Aparicio', '41008 Arroyo', 'Sevilla', '601070925', 'jesapaort');
 INSERT INTO owners VALUES (12, 'Alejandro', 'Barranco', '666 Reina Mercedes', 'Sevilla', '639058229', 'alebarled');
+INSERT INTO owners VALUES (13, 'Juan Ramón', 'Ostos', 'Rubio', '25 Sepulveda', 'Sevilla', '636363663', 'juanrauvu')
 
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (1, 'Leo', '2010-09-07', 1, 1);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (2, 'Basil', '2012-08-06', 6, 2);
@@ -71,6 +72,7 @@ INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (12, 'Lucky', '2010
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (13, 'Sly', '2012-06-08', 1, 10);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (14, 'Luffy', '2012-01-01', 2, 11);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (15, 'Rarmon', '2000-09-11', 2, 12);
+INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (16, 'BadJesus', '1990-08-30', 4, 13)
 
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (1, 7, '2013-01-01', 'rabies shot');
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (2, 8, '2013-01-02', 'rabies shot');
