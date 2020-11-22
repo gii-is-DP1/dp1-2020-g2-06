489517b587fcbc99d1ad1fd9d9f9cb83710a6c63
Juanra <juanramonor@hotmail.com>
2020-11-22 14:47:09 +0100

    Tutor 22-10-2020

---
+++ src/main/java/org/springframework/samples/petclinic/repository/TutorRepository.java
@@ -0,0 +1,20 @@
+package org.springframework.samples.petclinic.repository;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.repository.Repository;
+import org.springframework.samples.petclinic.model.Tutor;
+
+public interface TutorRepository extends Repository<Tutor, String> {
+
+	Collection<Tutor> findAll() throws DataAccessException;
+	
+	Optional<Tutor> findByEmail(String email) throws DataAccessException;
+	
+	void deleteByEmail(String email) throws DataAccessException;
+	
+	void save(Tutor tutor) throws DataAccessException;
+	
+}
