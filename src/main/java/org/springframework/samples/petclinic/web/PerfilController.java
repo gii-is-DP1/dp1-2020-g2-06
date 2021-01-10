c01c4b1b842a3bfe23e0bf200c3b2b46bc2e78b6
Jesus Aparicio <jesapaort@alum.us.es>
2021-01-10 19:41:43 +0100

    Implementación de perfil de Alumno,Creador y Tutor

---
+++ src/main/java/org/springframework/samples/petclinic/web/PerfilController.java
@@ -0,0 +1,58 @@
+package org.springframework.samples.petclinic.web;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.service.AlumnoService;
+import org.springframework.samples.petclinic.service.CreadorService;
+import org.springframework.samples.petclinic.service.ProblemaService;
+import org.springframework.samples.petclinic.service.TutorService;
+import org.springframework.samples.petclinic.util.Utils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/perfil")
+public class PerfilController {
+
+	@Autowired
+	ProblemaService problemaService;
+	
+	@Autowired
+	AlumnoService alumnoService;
+	
+	@Autowired
+	AlumnoController alumnoController;
+	
+	@Autowired
+	TutorController tutorController;
+	
+	@Autowired
+	TutorService tutorService;
+	
+	@Autowired
+	CreadorController creadorController;
+	
+	@Autowired
+	CreadorService creadorService;
+	
+	@GetMapping("")
+	public String PerfilRedirection(ModelMap model) throws IOException {
+		String email = SecurityContextHolder.getContext().getAuthentication().getName();
+		String tipoPerfil = Utils.authLoggedIn();
+		if(tipoPerfil.equals("alumno")) {
+			return alumnoController.alumnoDetails(alumnoService.findByEmail(email).get().getId(), model);
+		}else if(tipoPerfil.equals("tutor")){
+			return tutorController.tutorDetails(tutorService.findByEmail(email).get().getId(), 1, 1, model);
+		}else if(tipoPerfil.equals("creador")) {
+			return creadorController.creadorDetails(creadorService.findByEmail(email).get().getId(), model);
+		}else {
+			return "/welcome";
+		}
+		
+	}
+	
+}
