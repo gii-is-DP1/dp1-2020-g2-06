b66d19d5ddd4ba40c092273a554463b79b589e00
Barranco <barranco@barranco.es>
2021-02-05 10:53:08 +0100

    alumnoService commented

--- src/main/java/org/springframework/samples/petclinic/service/AlumnoService.java
+++ src/main/java/org/springframework/samples/petclinic/service/AlumnoService.java
@@ -7,31 +7,12 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.Properties;
-import java.util.stream.Collectors;
-
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.util.Pair;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.samples.petclinic.model.Alumno;
-import org.springframework.samples.petclinic.model.Articulo;
 import org.springframework.samples.petclinic.model.Problema;
 import org.springframework.samples.petclinic.repository.AlumnoRepository;
 import org.springframework.samples.petclinic.util.Utils;
@@ -136,39 +117,39 @@ public class AlumnoService {
 		return alumnoRepository.findByToken(confirmation_token);
 	}
 	
-	public void sendMail(Alumno alumno, JavaMailSender javaMailSender) throws AddressException, MessagingException {
-		String remitente = "information.codeus@gmail.com";
-		String destinatario = alumno.getEmail();
-		
-		String clave = "ypvf rrzj kkfm weqh";
-		String contraseña = "CodeUs2001DP1";
-		
-		String token = alumno.getNombre().substring(0, 3) + alumno.getApellidos().substring(0, 3) + alumno.getEmail().substring(4, 7) + "CDU1";
-		
-		Properties prop = new Properties();
-		prop.put("mail.smtp.host", "smtp.gmail.com");
-		prop.put("mail.smtp.port", "587");
-		prop.put("mail.smtp.auth", "true");
-		prop.put("mail.smtp.starttls.enable", "true");
-		prop.put("mail.smtp.user",remitente);
-		prop.put("mail.smtp.clave", contraseña);
-		
-		Session session = Session.getDefaultInstance(prop);
-		
-
-		//javaMailSender.send(msg);
-
-		try {
-			message.addRecipients(Message.RecipientType.TO, InternetAddress.parse(destinatario));
-			message.setSubject("Correo de verificación codeUs");
-			String msg = "Buenas " + alumno.getNombre() + ",\n" + "para poder acceder a codeUs, haz click en el siguiente enlace para verificar tu correo.\nhttp:localhost/alumnos/confirmation/" + token + " \n Gracias por unirte! Bienvenido!";
-			message.setText(msg);
-			Transport transport = session.getTransport("smtp");
-			transport.connect("smtp.gmail.com", remitente, contraseña);
-			transport.sendMessage(message, message.getAllRecipients());
-			transport.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+//	public void sendMail(Alumno alumno, JavaMailSender javaMailSender) throws AddressException, MessagingException {
+//		String remitente = "information.codeus@gmail.com";
+//		String destinatario = alumno.getEmail();
+//		
+//		String clave = "ypvf rrzj kkfm weqh";
+//		String contraseña = "CodeUs2001DP1";
+//		
+//		String token = alumno.getNombre().substring(0, 3) + alumno.getApellidos().substring(0, 3) + alumno.getEmail().substring(4, 7) + "CDU1";
+//		
+//		Properties prop = new Properties();
+//		prop.put("mail.smtp.host", "smtp.gmail.com");
+//		prop.put("mail.smtp.port", "587");
+//		prop.put("mail.smtp.auth", "true");
+//		prop.put("mail.smtp.starttls.enable", "true");
+//		prop.put("mail.smtp.user",remitente);
+//		prop.put("mail.smtp.clave", contraseña);
+//		
+//		Session session = Session.getDefaultInstance(prop);
+//		
+//
+//		//javaMailSender.send(msg);
+//
+//		try {
+//			message.addRecipients(Message.RecipientType.TO, InternetAddress.parse(destinatario));
+//			message.setSubject("Correo de verificación codeUs");
+//			String msg = "Buenas " + alumno.getNombre() + ",\n" + "para poder acceder a codeUs, haz click en el siguiente enlace para verificar tu correo.\nhttp:localhost/alumnos/confirmation/" + token + " \n Gracias por unirte! Bienvenido!";
+//			message.setText(msg);
+//			Transport transport = session.getTransport("smtp");
+//			transport.connect("smtp.gmail.com", remitente, contraseña);
+//			transport.sendMessage(message, message.getAllRecipients());
+//			transport.close();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 }
